7956002ad1a991e01e071cb9407ab0b5528397d6
Robert's PC <robertting465100@hotmail.com>
2022-05-15 00:30:18 +0800

    Order

--- app/src/main/java/com/example/order/Dessert.java
+++ app/src/main/java/com/example/order/Dessert.java
@@ -3,13 +3,12 @@ package com.example.order;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class Meat extends AppCompatActivity {
+public class Dessert extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_meat);
+        setContentView(R.layout.activity_dessert);
     }
 }
